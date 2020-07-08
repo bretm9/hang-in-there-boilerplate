7e173a1265d033ee954b39f43584195e2eb4bb99
Bret Merritt <merrittbret9@gmail.com>
2020-07-08 17:20:50 -0600

    Add random image, title, and quote functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // query selector variables go here 👇
-
+var posterImg = document.querySelector(".poster-img");
+var posterTitle = document.querySelector(".poster-title")
+var posterQuote = document.querySelector(".poster-quote");
 // we've provided you with some data to work with 👇
 var images = [
   "./assets/bees.jpg",
@@ -98,20 +100,24 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
-var savedPosters = [
-  makePoster(
-    "https://i.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif",
-    "Optimism",
-    "Keep a joyful heart!"
-  )
-];
+// var savedPosters = [
+//   makePoster(
+//     "https://i.giphy.com/media/5LU6ZcEGBbhVS/giphy.gif",
+//     "Optimism",
+//     "Keep a joyful heart!"
+//   )
+// ];
 var currentPoster;
 
 // event listeners go here 👇
 
 // functions and event handlers go here 👇
+
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
+posterImg.src = images[getRandomIndex(images)];
+posterTitle.innerText = titles[getRandomIndex(titles)];
+posterQuote.innerText = quotes[getRandomIndex(quotes)];
